7a7626c3124375add4e29f6895cf1e8183dad3ef
Param Savjani <paramsavjani3010@gmail.com>
2025-12-14 17:55:26 +0530

    Add PostgreSQL dependency and configure datasource in application.yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.postgresql:postgresql")
 }
 
 kotlin {

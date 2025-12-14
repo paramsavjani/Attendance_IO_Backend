fd1a1ea14b4159d649f31d503725f48a8b59d7cb
Param Savjani <paramsavjani3010@gmail.com>
2025-12-14 17:37:04 +0530

    Init

---
+++ src/main/kotlin/com/underworldnetwork/api/UnderworldnetworkBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.underworldnetwork.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UnderworldnetworkBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<UnderworldnetworkBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/underworldnetwork/api/UnderworldnetworkBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.underworldnetwork.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnderworldnetworkBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

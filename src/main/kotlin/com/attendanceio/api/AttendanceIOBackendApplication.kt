b4f23d23b4c94822bab5b81441d313d8ba024bc6
Param Savjani <paramsavjani3010@gmail.com>
2025-12-21 20:13:47 +0530

    added time zone to the application

--- src/main/kotlin/com/attendanceio/api/AttendanceIOBackendApplication.kt
+++ src/main/kotlin/com/attendanceio/api/AttendanceIOBackendApplication.kt
@@ -3,10 +3,19 @@ package com.attendanceio.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.TimeZone
+import jakarta.annotation.PostConstruct
 
 @SpringBootApplication
 @EnableScheduling
-class AttendanceIOBackendApplication
+class AttendanceIOBackendApplication {
+	
+	@PostConstruct
+	fun init() {
+		// Set default timezone to IST (Asia/Kolkata) for all scheduled tasks and date/time operations
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"))
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<AttendanceIOBackendApplication>(*args)
